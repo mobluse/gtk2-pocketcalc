ba183f1e3eb8fbe78725b37b2db433330ae2fbab
Mikael Bonnier <mikael.bonnier@gmail.com>
2015-06-08 01:47:17 +0200

    Update.

--- pocketcalc.cs
+++ pocketcalc.cs
@@ -1,201 +1,203 @@
-// pocketcalc.cs - A pocket calculator for GTK#
+// pocketcalc.cs - An unfinished pocket calculator for GTK#
 // Author: Mikael O. Bonnier <mikael.bonnier@gmail.com>
 // License: GPLv3+
+// Install on Linux: sudo apt-get install mono-complete gtk-sharp2
+// Build: mcs pocketcalc.cs -pkg:gtk-sharp-2.0
+// Run: mono pocketcalc.exe (or ./pocketcalc.exe)
+// See: http://www.maketecheasier.com/write-c-sharp-programs-raspberry-pi/
+//   http://www.mono-project.com/docs/gui/gtksharp/installer-for-net-framework/
+//   https://scratch.mit.edu/projects/13110194/
 
-namespace GtkSamples {
+using Gtk;
+//using System;
 
-	using Gtk;
-	using System;
+public class PocketCalc  {
 
-	public class PocketCalc  {
+  public static Entry display;
 
-		public static Entry display;
+  public static int Main (string[] args)
+  {
+    Application.Init ();
+    Window win = new Window ("PocketCalc");
+    win.DeleteEvent += new DeleteEventHandler (Window_Delete);
+    win.BorderWidth = 5;
+    win.SetPosition (WindowPosition.CenterAlways);
 
-		public static int Main (string[] args)
-		{
-			Application.Init ();
-			Window win = new Window ("PocketCalc");
-			win.DeleteEvent += new DeleteEventHandler (Window_Delete);
-			win.BorderWidth = 5;
-			win.SetPosition (WindowPosition.CenterAlways);
+    Table table = new Table (7, 5, false);
 
-			Table table = new Table (7, 5, false);
+    Button btn_x = new Button ("X");
+    table.Attach(btn_x, 0, 1, 0, 1);
 
-			Button btn_x = new Button ("X");
-			table.Attach(btn_x, 0, 1, 0, 1);
+    display = new Entry ("0");
+    table.Attach(display, 1, 6, 0, 1);
 
-			display = new Entry ("0");
-			table.Attach(display, 1, 6, 0, 1);
+    Button btn_ms = new Button ("MS");
+    table.Attach(btn_ms, 6, 7, 0, 1);
 
-			Button btn_ms = new Button ("MS");
-			table.Attach(btn_ms, 6, 7, 0, 1);
+    Button btn_neg = new Button ("±");
+    table.Attach(btn_neg, 0, 1, 1, 2);			
 
-			Button btn_neg = new Button ("±");
-			table.Attach(btn_neg, 0, 1, 1, 2);			
+    Button btn_7 = new Button ("7");
+    btn_7.Clicked += new EventHandler (rec_7);
+    table.Attach(btn_7, 1, 2, 1, 2);			
 
-			Button btn_7 = new Button ("7");
-			btn_7.Clicked += new EventHandler (rec_7);
-			table.Attach(btn_7, 1, 2, 1, 2);			
+    Button btn_8 = new Button ("8");
+    btn_8.Clicked += new EventHandler (rec_8);
+    table.Attach(btn_8, 2, 3, 1, 2);			
 
-			Button btn_8 = new Button ("8");
-			btn_8.Clicked += new EventHandler (rec_8);
-			table.Attach(btn_8, 2, 3, 1, 2);			
+    Button btn_9 = new Button ("9");
+    btn_9.Clicked += new EventHandler (rec_9);
+    table.Attach(btn_9, 3, 4, 1, 2);			
 
-			Button btn_9 = new Button ("9");
-			btn_9.Clicked += new EventHandler (rec_9);
-			table.Attach(btn_9, 3, 4, 1, 2);			
+    Button btn_pct = new Button ("%");
+    table.Attach(btn_pct, 4, 5, 1, 2);			
 
-			Button btn_pct = new Button ("%");
-			table.Attach(btn_pct, 4, 5, 1, 2);			
+    Button btn_q = new Button ("?");
+    table.Attach(btn_q, 5, 6, 1, 2);			
 
-			Button btn_q = new Button ("?");
-			table.Attach(btn_q, 5, 6, 1, 2);			
+    Button btn_mc = new Button ("MC");
+    table.Attach(btn_mc, 6, 7, 1, 2);			
 
-			Button btn_mc = new Button ("MC");
-			table.Attach(btn_mc, 6, 7, 1, 2);			
+    Button btn_sqrt = new Button ("√");
+    table.Attach(btn_sqrt, 0, 1, 2, 3);			
 
-			Button btn_sqrt = new Button ("√");
-			table.Attach(btn_sqrt, 0, 1, 2, 3);			
+    Button btn_4 = new Button ("4");
+    btn_4.Clicked += new EventHandler (rec_4);
+    table.Attach(btn_4, 1, 2, 2, 3);			
 
-			Button btn_4 = new Button ("4");
-			btn_4.Clicked += new EventHandler (rec_4);
-			table.Attach(btn_4, 1, 2, 2, 3);			
+    Button btn_5 = new Button ("5");
+    btn_5.Clicked += new EventHandler (rec_5);
+    table.Attach(btn_5, 2, 3, 2, 3);			
 
-			Button btn_5 = new Button ("5");
-			btn_5.Clicked += new EventHandler (rec_5);
-			table.Attach(btn_5, 2, 3, 2, 3);			
+    Button btn_6 = new Button ("6");
+    btn_6.Clicked += new EventHandler (rec_6);
+    table.Attach(btn_6, 3, 4, 2, 3);			
 
-			Button btn_6 = new Button ("6");
-			btn_6.Clicked += new EventHandler (rec_6);
-			table.Attach(btn_6, 3, 4, 2, 3);			
+    Button btn_mul = new Button ("×");
+    table.Attach(btn_mul, 4, 5, 2, 3);			
 
-			Button btn_mul = new Button ("×");
-			table.Attach(btn_mul, 4, 5, 2, 3);			
+    Button btn_div = new Button ("÷");
+    table.Attach(btn_div, 5, 6, 2, 3);
 
-			Button btn_div = new Button ("÷");
-			table.Attach(btn_div, 5, 6, 2, 3);
+    Button btn_mr = new Button ("MR");
+    table.Attach(btn_mr, 6, 7, 2, 3);			
 
-			Button btn_mr = new Button ("MR");
-			table.Attach(btn_mr, 6, 7, 2, 3);			
+    Button btn_c = new Button ("C");
+    btn_c.Clicked += new EventHandler (rec_c);
+    table.Attach(btn_c, 0, 1, 3, 4);			
 
-			Button btn_c = new Button ("C");
-			btn_c.Clicked += new EventHandler (rec_c);
-			table.Attach(btn_c, 0, 1, 3, 4);			
+    Button btn_1 = new Button ("1");
+    btn_1.Clicked += new EventHandler (rec_1);
+    table.Attach(btn_1, 1, 2, 3, 4);			
 
-			Button btn_1 = new Button ("1");
-			btn_1.Clicked += new EventHandler (rec_1);
-			table.Attach(btn_1, 1, 2, 3, 4);			
+    Button btn_2 = new Button ("2");
+    btn_2.Clicked += new EventHandler (rec_2);
+    table.Attach(btn_2, 2, 3, 3, 4);			
 
-			Button btn_2 = new Button ("2");
-			btn_2.Clicked += new EventHandler (rec_2);
-			table.Attach(btn_2, 2, 3, 3, 4);			
+    Button btn_3 = new Button ("3");
+    btn_3.Clicked += new EventHandler (rec_3);
+    table.Attach(btn_3, 3, 4, 3, 4);			
 
-			Button btn_3 = new Button ("3");
-			btn_3.Clicked += new EventHandler (rec_3);
-			table.Attach(btn_3, 3, 4, 3, 4);			
+    Button btn_add = new Button ("+");
+    table.Attach(btn_add, 4, 5, 3, 5);
 
-			Button btn_add = new Button ("+");
-			table.Attach(btn_add, 4, 5, 3, 5);
+    Button btn_sub = new Button ("-");
+    table.Attach(btn_sub, 5, 6, 3, 4);
 
-			Button btn_sub = new Button ("-");
-			table.Attach(btn_sub, 5, 6, 3, 4);
+    Button btn_m_sub = new Button ("M-");
+    table.Attach(btn_m_sub, 6, 7, 3, 4);
 
-			Button btn_m_sub = new Button ("M-");
-			table.Attach(btn_m_sub, 6, 7, 3, 4);
+    Button btn_ac = new Button ("AC");
+    btn_ac.Clicked += new EventHandler (rec_ac);
+    table.Attach(btn_ac, 0, 1, 4, 5);
 
-			Button btn_ac = new Button ("AC");
-			btn_ac.Clicked += new EventHandler (rec_ac);
-			table.Attach(btn_ac, 0, 1, 4, 5);
+    Button btn_0 = new Button ("0");
+    btn_0.Clicked += new EventHandler (rec_0);
+    table.Attach(btn_0, 1, 3, 4, 5);
 
-			Button btn_0 = new Button ("0");
-			btn_0.Clicked += new EventHandler (rec_0);
-			table.Attach(btn_0, 1, 3, 4, 5);
+    Button btn_pt = new Button (".");
+    table.Attach(btn_pt, 3, 4, 4, 5);
 
-			Button btn_pt = new Button (".");
-			table.Attach(btn_pt, 3, 4, 4, 5);
+    Button btn_eq = new Button ("=");
+    table.Attach(btn_eq, 5, 6, 4, 5);			
 
-			Button btn_eq = new Button ("=");
-			table.Attach(btn_eq, 5, 6, 4, 5);			
+    Button btn_m_add = new Button ("M+");
+    table.Attach(btn_m_add, 6, 7, 4, 5);			
 
-			Button btn_m_add = new Button ("M+");
-			table.Attach(btn_m_add, 6, 7, 4, 5);			
+    win.Add (table);
+    win.ShowAll ();
+    Application.Run ();
+    return 0;
+  }
 
-			win.Add (table);
-			win.ShowAll ();
-			Application.Run ();
-			return 0;
-		}
+  static void Window_Delete (object obj, DeleteEventArgs args)
+  {
+    Application.Quit ();
+    args.RetVal = true;
+  }
 
-		static void Window_Delete (object obj, DeleteEventArgs args)
-		{
-			Application.Quit ();
-			args.RetVal = true;
-		}
+  static void rec_7 (object obj, EventArgs args)
+  {
+    append("7");
+  }
 
-		static void rec_7 (object obj, EventArgs args)
-		{
-			append("7");
-		}
+  static void rec_8 (object obj, EventArgs args)
+  {
+    append("8");
+  }
 
-		static void rec_8 (object obj, EventArgs args)
-		{
-			append("8");
-		}
+  static void rec_9 (object obj, EventArgs args)
+  {
+    append("9");
+  }
 
-		static void rec_9 (object obj, EventArgs args)
-		{
-			append("9");
-		}
+  static void rec_4 (object obj, EventArgs args)
+  {
+    append("4");
+  }
 
-		static void rec_4 (object obj, EventArgs args)
-		{
-			append("4");
-		}
+  static void rec_5 (object obj, EventArgs args)
+  {
+    append("5");
+  }
 
-		static void rec_5 (object obj, EventArgs args)
-		{
-			append("5");
-		}
+  static void rec_6 (object obj, EventArgs args)
+  {
+    append("6");
+  }
 
-		static void rec_6 (object obj, EventArgs args)
-		{
-			append("6");
-		}
+  static void rec_c (object obj, EventArgs args)
+  {
+    display.Text = "0";
+  }
 
-		static void rec_c (object obj, EventArgs args)
-		{
-			display.Text = "0";
-		}
+  static void rec_1 (object obj, EventArgs args)
+  {
+    append("1");
+  }
 
-		static void rec_1 (object obj, EventArgs args)
-		{
-			append("1");
-		}
+  static void rec_2 (object obj, EventArgs args)
+  {
+    append("2");
+  }
 
-		static void rec_2 (object obj, EventArgs args)
-		{
-			append("2");
-		}
+  static void rec_3 (object obj, EventArgs args)
+  {
+    append("3");
+  }
 
-		static void rec_3 (object obj, EventArgs args)
-		{
-			append("3");
-		}
+  static void rec_ac (object obj, EventArgs args)
+  {
+    display.Text = "0";
+  }
 
-		static void rec_ac (object obj, EventArgs args)
-		{
-			display.Text = "0";
-		}
+  static void rec_0 (object obj, EventArgs args)
+  {
+    append("0");
+  }
 
-		static void rec_0 (object obj, EventArgs args)
-		{
-			append("0");
-		}
-
-		static void append (String number)
-		{
-			display.Text = display.Text + number;
-		}
-
-	}
+  static void append (String number)
+  {
+    display.Text = display.Text + number;
+  }
 }
--- pocketcalc.pl
+++ pocketcalc.pl
@@ -1,154 +1,156 @@
 #!/usr/bin/perl
-# pocketcalc.pl - A pocket calculator for GTK2
+# pocketcalc.pl - An unfinished pocket calculator for Gtk2
 # Author: Mikael O. Bonnier <mikael.bonnier@gmail.com>
 # License: GPLv3+
+# Install on Linux: sudo apt-get install libgtk2-perl
+# Run: perl pocketcalc.pl (or ./pocketcalc.pl)
+# See: http://gtk2-perl.sourceforge.net/doc/gtk2-perl-study-guide/
+#      http://gtk2-perl.sourceforge.net/win32/
+#      https://scratch.mit.edu/projects/13110194/
 
 use strict;
 use Glib qw/TRUE FALSE/;
 use Gtk2 -init;
 use utf8;
 
-my $window = Gtk2::Window->new('toplevel');
-$window->signal_connect('delete-event'=> sub {Gtk2->main_quit()});
-$window->set_title('PocketCalc');
-$window->set_border_width(5);
-$window->set_position('center-always');
+my $win = Gtk2::Window->new('toplevel');
+$win->signal_connect('delete-event'=> sub {Gtk2->main_quit()});
+$win->set_title('PocketCalc');
+$win->set_border_width(5);
+$win->set_position('center-always');
 
 my $display;
 
-&fill_window($window);
-
-$window->show_all();
-
-Gtk2->main();
-
-sub fill_window {
-  my ($win) = @_;
-
-  my $table = Gtk2::Table->new(7, 5, FALSE);
+sub fill_table {
+  my $tbl = Gtk2::Table->new(7, 5, FALSE);
   
   my $btn_x = Gtk2::Button->new('X');
   $btn_x->signal_connect('button-release-event' => \&rec_x);
-  $table->attach_defaults ($btn_x, 0, 1, 0, 1);
+  $tbl->attach_defaults ($btn_x, 0, 1, 0, 1);
 
   $display = Gtk2::Entry->new();
   $display->set_text('0');
   $display->signal_connect(changed => sub {
     my $text = $display->get_text;
   });
-  $table->attach_defaults($display, 1, 6, 0, 1);
+  $tbl->attach_defaults($display, 1, 6, 0, 1);
 
   my $btn_ms = Gtk2::Button->new('MS');
   $btn_ms->signal_connect('button-release-event' => \&rec_ms);
-  $table->attach_defaults ($btn_ms, 6, 7, 0, 1);
+  $tbl->attach_defaults ($btn_ms, 6, 7, 0, 1);
 
   my $btn_neg = Gtk2::Button->new('±');
   $btn_neg->signal_connect('button-release-event' => \&rec_neg);
-  $table->attach_defaults ($btn_neg, 0, 1, 1, 2);
+  $tbl->attach_defaults ($btn_neg, 0, 1, 1, 2);
 
   my $btn_7 = Gtk2::Button->new('7');
   $btn_7->signal_connect('button-release-event' => \&rec_7);
-  $table->attach_defaults ($btn_7, 1, 2, 1, 2);
+  $tbl->attach_defaults ($btn_7, 1, 2, 1, 2);
 
   my $btn_8 = Gtk2::Button->new('8');
   $btn_8->signal_connect('button-release-event' => \&rec_8);
-  $table->attach_defaults ($btn_8, 2, 3, 1, 2);
+  $tbl->attach_defaults ($btn_8, 2, 3, 1, 2);
 
   my $btn_9 = Gtk2::Button->new('9');
   $btn_9->signal_connect('button-release-event' => \&rec_9);
-  $table->attach_defaults ($btn_9, 3, 4, 1, 2);
+  $tbl->attach_defaults ($btn_9, 3, 4, 1, 2);
 
   my $btn_pct = Gtk2::Button->new('%');
   $btn_pct->signal_connect('button-release-event' => \&rec_pct);
-  $table->attach_defaults ($btn_pct, 4, 5, 1, 2);
+  $tbl->attach_defaults ($btn_pct, 4, 5, 1, 2);
 
   my $btn_q = Gtk2::Button->new('?');
   $btn_q->signal_connect('button-release-event' => \&rec_q);
-  $table->attach_defaults ($btn_q, 5, 6, 1, 2);
+  $tbl->attach_defaults ($btn_q, 5, 6, 1, 2);
 
   my $btn_mc = Gtk2::Button->new('MC');
   $btn_mc->signal_connect('button-release-event' => \&rec_mc);
-  $table->attach_defaults ($btn_mc, 6, 7, 1, 2);
+  $tbl->attach_defaults ($btn_mc, 6, 7, 1, 2);
 
   my $btn_sqrt = Gtk2::Button->new('√');
   $btn_sqrt->signal_connect('button-release-event' => \&rec_sqrt);
-  $table->attach_defaults ($btn_sqrt, 0, 1, 2, 3);
+  $tbl->attach_defaults ($btn_sqrt, 0, 1, 2, 3);
 
   my $btn_4 = Gtk2::Button->new('4');
   $btn_4->signal_connect('button-release-event' => \&rec_4);
-  $table->attach_defaults ($btn_4, 1, 2, 2, 3);
+  $tbl->attach_defaults ($btn_4, 1, 2, 2, 3);
 
   my $btn_5 = Gtk2::Button->new('5');
   $btn_5->signal_connect('button-release-event' => \&rec_5);
-  $table->attach_defaults ($btn_5, 2, 3, 2, 3);
+  $tbl->attach_defaults ($btn_5, 2, 3, 2, 3);
 
   my $btn_6 = Gtk2::Button->new('6');
   $btn_6->signal_connect('button-release-event' => \&rec_6);
-  $table->attach_defaults ($btn_6, 3, 4, 2, 3);
+  $tbl->attach_defaults ($btn_6, 3, 4, 2, 3);
 
   my $btn_mul = Gtk2::Button->new('×');
   $btn_mul->signal_connect('button-release-event' => \&rec_mul);
-  $table->attach_defaults ($btn_mul, 4, 5, 2, 3);
+  $tbl->attach_defaults ($btn_mul, 4, 5, 2, 3);
 
   my $btn_div = Gtk2::Button->new('÷');
   $btn_div->signal_connect('button-release-event' => \&rec_div);
-  $table->attach_defaults ($btn_div, 5, 6, 2, 3);
+  $tbl->attach_defaults ($btn_div, 5, 6, 2, 3);
 
   my $btn_mr = Gtk2::Button->new('MR');
   $btn_mr->signal_connect('button-release-event' => \&rec_mr);
-  $table->attach_defaults ($btn_mr, 6, 7, 2, 3);
+  $tbl->attach_defaults ($btn_mr, 6, 7, 2, 3);
 
   my $btn_c = Gtk2::Button->new('C');
   $btn_c->signal_connect('button-release-event' => \&rec_c);
-  $table->attach_defaults ($btn_c, 0, 1, 3, 4);
+  $tbl->attach_defaults ($btn_c, 0, 1, 3, 4);
 
   my $btn_1 = Gtk2::Button->new('1');
   $btn_1->signal_connect('button-release-event' => \&rec_1);
-  $table->attach_defaults ($btn_1, 1, 2, 3, 4);
+  $tbl->attach_defaults ($btn_1, 1, 2, 3, 4);
 
   my $btn_2 = Gtk2::Button->new('2');
   $btn_2->signal_connect('button-release-event' => \&rec_2);
-  $table->attach_defaults ($btn_2, 2, 3, 3, 4);
+  $tbl->attach_defaults ($btn_2, 2, 3, 3, 4);
 
   my $btn_3 = Gtk2::Button->new('3');
   $btn_3->signal_connect('button-release-event' => \&rec_3);
-  $table->attach_defaults ($btn_3, 3, 4, 3, 4);
+  $tbl->attach_defaults ($btn_3, 3, 4, 3, 4);
 
   my $btn_add = Gtk2::Button->new('+');
   $btn_add->signal_connect('button-release-event' => \&rec_add);
-  $table->attach_defaults ($btn_add, 4, 5, 3, 5);
+  $tbl->attach_defaults ($btn_add, 4, 5, 3, 5);
 
   my $btn_sub = Gtk2::Button->new('-');
   $btn_sub->signal_connect('button-release-event' => \&rec_sub);
-  $table->attach_defaults ($btn_sub, 5, 6, 3, 4);
+  $tbl->attach_defaults ($btn_sub, 5, 6, 3, 4);
 
   my $btn_m_sub = Gtk2::Button->new('M-');
   $btn_m_sub->signal_connect('button-release-event' => \&rec_m_sub);
-  $table->attach_defaults ($btn_m_sub, 6, 7, 3, 4);
+  $tbl->attach_defaults ($btn_m_sub, 6, 7, 3, 4);
 
   my $btn_ac = Gtk2::Button->new('AC');
   $btn_ac->signal_connect('button-release-event' => \&rec_ac);
-  $table->attach_defaults ($btn_ac, 0, 1, 4, 5);
+  $tbl->attach_defaults ($btn_ac, 0, 1, 4, 5);
 
   my $btn_0 = Gtk2::Button->new('0');
   $btn_0->signal_connect('button-release-event' => \&rec_0);
-  $table->attach_defaults ($btn_0, 1, 3, 4, 5);
+  $tbl->attach_defaults ($btn_0, 1, 3, 4, 5);
 
   my $btn_pt = Gtk2::Button->new('.');
   $btn_pt->signal_connect('button-release-event' => \&rec_pt);
-  $table->attach_defaults ($btn_pt, 3, 4, 4, 5);
+  $tbl->attach_defaults ($btn_pt, 3, 4, 4, 5);
 
   my $btn_eq = Gtk2::Button->new('=');
   $btn_eq->signal_connect('button-release-event' => \&rec_eq);
-  $table->attach_defaults ($btn_eq, 5, 6, 4, 5);
+  $tbl->attach_defaults ($btn_eq, 5, 6, 4, 5);
 
   my $btn_m_add = Gtk2::Button->new('M+');
   $btn_m_add->signal_connect('button-release-event' => \&rec_m_add);
-  $table->attach_defaults ($btn_m_add, 6, 7, 4, 5);
+  $tbl->attach_defaults ($btn_m_add, 6, 7, 4, 5);
 
-  $win->add($table);
-  return $win;
+  return $tbl;
 }
+
+my $table = fill_table();
+$win->add($table);
+$win->show_all();
+
+Gtk2->main();
 
 sub rec_x {
 
